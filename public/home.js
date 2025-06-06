2036db7015205b39b4b2f4eeb35eb44ad0048297
Saddy14 <1221301874@student.mmu.edu.my>
2025-06-07 06:13:16 +0800

    Enhance project card display and layout: add dynamic dataset rendering, improve button layout, and update styles for better user experience.

--- public/home.js
+++ public/home.js
@@ -11,3 +11,36 @@ window.addEventListener('DOMContentLoaded', async () => {
         }
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('/api/pinataFiles')
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById("project-grid");
+            container.innerHTML = ""; // Clear any placeholder
+
+            data.files.forEach(file => {
+                const isEncrypted = file.keyvalues?.EncryptedFile === "1";
+                const fileName = file.name.split('--')[1]
+                const card = document.createElement("div");
+                card.className = "project-card";
+
+                card.innerHTML = `
+                    <h3>${fileName}</h3>
+                    <p>${file.keyvalues?.Description || "N/A"}</p>
+                    <p>Encrypted: ${isEncrypted ? "Yes" : "No"}</p>
+                    <p>Created at: ${new Date(file.created_at).toLocaleString()}</p>
+                    <p >Owner: ${file.keyvalues?.WalletAddress}</p>
+                    <div class="button-container">
+                        <button onclick="window.open('https://purple-wrong-ferret-861.mypinata.cloud/ipfs/${file.cid}', '_blank')" class="projectbtn">Download Data</button>
+                        
+                    </div>
+                `;
+
+                container.appendChild(card);
+            });
+        })
+        .catch(error => {
+            console.error("Failed to load datasets:", error);
+        });
+})
